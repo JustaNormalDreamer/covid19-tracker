5ed6be959fc8f4ac377b0c63ca9e376d6c059222
Dipesh <team7hookage@gmail.com>
2020-04-19 10:56:06 +0545

    Charts done!

---
+++ precache-manifest.809e724ad019725998f5bed861d2045c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6acc8c275652d0cc35f8745e201a4f27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90118157e9021ee144b8",
+    "url": "/static/css/main.d946f033.chunk.css"
+  },
+  {
+    "revision": "3537ffccb575d90053b2",
+    "url": "/static/js/2.c8798838.chunk.js"
+  },
+  {
+    "revision": "90118157e9021ee144b8",
+    "url": "/static/js/main.d14b5913.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
