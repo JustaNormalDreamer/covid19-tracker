201c57645338537778c28500256bbafa0edec693
Dipesh <team7hookage@gmail.com>
2020-04-19 11:09:28 +0545

    Updates

---
+++ precache-manifest.eb71b3763ba3da96d4a51b50ed173cc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf16aeab6129c2fed9e17aa9cb3c4682",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "6faf66ef878b061c7d16",
+    "url": "/covid19-tracker/static/css/main.ecbdf50e.chunk.css"
+  },
+  {
+    "revision": "5266105383ca23488041",
+    "url": "/covid19-tracker/static/js/2.afb6f909.chunk.js"
+  },
+  {
+    "revision": "5eefe3d988907fb680c439c15ee4c323",
+    "url": "/covid19-tracker/static/js/2.afb6f909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6faf66ef878b061c7d16",
+    "url": "/covid19-tracker/static/js/main.48aabaf6.chunk.js"
+  },
+  {
+    "revision": "eb5ff0e51105f2f9eece",
+    "url": "/covid19-tracker/static/js/runtime-main.782e1f46.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
